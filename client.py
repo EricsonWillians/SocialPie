8d7e98dd10b2b77aa6c08b17a3529d9c082cac2f
EricsonWillians <rederickdeathwill@gmail.com>
2016-04-25 04:17:26 -0300

    Fixed widget adjustment to window resize.

--- client.py
+++ client.py
@@ -35,34 +35,45 @@ class App(threading.Thread):
 	def callback(self):
 		self.root.quit()
 		
+	def center(self, win):
+		win.update_idletasks()
+		width = win.winfo_width()
+		height = win.winfo_height()
+		x = (win.winfo_screenwidth() // 2) - (width // 2)
+		y = (win.winfo_screenheight() // 2) - (height // 2)
+		win.geometry("{}x{}+{}+{}".format(width, height, x, y))
+		
 	def run(self):
 		self.root = tk.Tk()
 		self.root.protocol("WM_DELETE_WINDOW", self.callback)
 		self.root.title("Social Pie Client")
 		self.root.grid()
-		self.root.grid_columnconfigure(1, weight=1)
+		self.root.grid_columnconfigure(0, weight=1)
+		for n in range(7):
+			self.root.grid_rowconfigure(n, weight=2)
 		self.create_widgets()
 		self.console.insert(tk.END, "Social Pie initiated.\n")
 		self.connected = False
+		self.center(self.root)
 		self.root.mainloop()
 	
 	def create_widgets(self):
 		self.host_label = tk.Label(self.root, text="Host: ")
-		self.host_label.grid(column=0, row=0, padx=270, pady=10, columnspan=2, sticky="w")
+		self.host_label.grid(column=0, row=0, padx=0, pady=10, sticky=tk.W+tk.E)
 		self.host_entry = tk.Entry(self.root, bg="#000", fg="#0F0", highlightcolor="#F00", highlightthickness=2, takefocus=1)
-		self.host_entry.grid(column=0, row=0, pady=10, columnspan=2)
+		self.host_entry.grid(column=0, row=1, padx=25, pady=10, sticky=tk.W+tk.E)
 		self.host_entry.bind("<Return>", self.connect)
 		self.console = tk.Text(self.root, bg="#000", fg="#0F0", highlightcolor="#F00", highlightthickness=2)
-		self.console.grid(column=0, row=1, padx=20, columnspan=2)
+		self.console.grid(column=0, row=2, padx=25, pady=10, sticky=tk.W+tk.E)
 		self.msg_label = tk.Label(self.root, text="Message: ")
-		self.msg_label.grid(column=0, row=2, padx=25)
+		self.msg_label.grid(column=0, row=3, pady=10, sticky=tk.W+tk.E)
 		self.msg_area = tk.Text(self.root, height=6, width=58, bg="#000", fg="#0F0", highlightcolor="#F00", highlightthickness=2)
-		self.msg_area.grid(column=0, row=3, ipadx=10, padx=25, pady=10, columnspan=2, sticky="w")
+		self.msg_area.grid(column=0, row=4, padx=25, pady=10, sticky=tk.W+tk.E)
 		self.msg_area.bind("<Return>", self.send)
 		self.connect_button = tk.Button(self.root, text="Connect", command=self.connect)
-		self.connect_button.grid(column=0, row=3, ipadx=10, padx=85, pady=10, columnspan=2, sticky="es")
+		self.connect_button.grid(column=0, row=5, padx=30, pady=10, sticky=tk.W)
 		self.quit_button = tk.Button(self.root, text="Exit", command=self.root.quit)
-		self.quit_button.grid(column=1, row=3, ipadx=10, padx=30, pady=10, sticky="es")
+		self.quit_button.grid(column=0, row=5, padx=110, pady=10, sticky=tk.W)
 		self.host_entry.focus_set()
 	
 	def send(self, x=None):
@@ -83,10 +94,9 @@ class App(threading.Thread):
 				
 			except Exception as e:
 				self.console.insert(tk.END, "Error trying to connect to {host}: {msg}\n".format(host = self.host, msg = str(e)))
-			
 		else:
-		#	self.sock.close()
-		#	self.rsock.close()
+			self.sock.close()
+			self.rsock.close()
 			self.console.insert(tk.END, "Connection with {host} destroyed.\n".format(host = self.host))
 			self.connect_button.config(text="Connect")
 			self.connected = False
@@ -99,4 +109,3 @@ if __name__ == '__main__':
 			app.console.insert(tk.END, "{msg}.".format(msg = received))
 		except:
 			pass
-		
